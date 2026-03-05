db12fccab8ceea0333e4f9b814a49a7381c597b3
kenwu020902 <kenwu020902@gmail.com>
2026-03-05 13:06:55 +0000

    Create ShapeType.cs

---
+++ ShapeType.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+// This defines all possible shapes in the game
+public enum ShapeType
+{
+    Triangle,    // 🔺
+    Square,      // ⬛
+    Rectangle,   // 🔵
+    Round,       // ⚪
+    Star,        // ⭐
+    Hexagon      // 🔷
+}
